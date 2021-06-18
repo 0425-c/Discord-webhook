056d22bddfffdaa45ba433d48ba2bc41aaadc9f7
0425-c <84583638+0425-c@users.noreply.github.com>
2021-06-18 20:23:24 +0900

    Add files via upload

---
+++ Webhook_/Webhook_/Function.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Web;
+
+namespace Webhook_
+{
+    internal class Functions
+    {
+        private static byte[] Post(string url, NameValueCollection pairs)
+        {
+            byte[] result;
+            using (WebClient webClient = new WebClient())
+            {
+                result = webClient.UploadValues(url, pairs);
+            }
+            return result;
+        }
+
+        public static void Promotion(string URLs, string Content, string SenderName)
+        {
+
+            Functions.SendWebhook(URLs, Content, SenderName);
+
+        }
+
+        private static void SendWebhook(string URL, string content, string username)
+        {
+            try
+            {
+                Functions.Post(URL, new NameValueCollection
+                {
+                    {
+                        "username",
+                        username
+                    },
+                    {
+                        "content",
+                        content
+                    }
+                });
+            }
+            catch
+            { }
+        }
+        public Functions()
+        {
+        }
+    }
+}
+
